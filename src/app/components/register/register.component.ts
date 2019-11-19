244ec56d863191f2e6cc899233b77246e2d87198
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-11-18 21:56:41 -0700

    Servicio de usuarios y registro.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import {UserService} from '../../services/user.service';
+
+
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   public page_title: string;
   public user: User;
+  public status: string;
+  constructor(
+    private _userService: UserService
 
-  constructor() {
+  ) {
     this.page_title = 'Registrate';
     this.user = new User(1, '', '','ROLE_USER','','','','');
    }
@@ -19,7 +26,23 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(form){
-    console.log(this.user);
-    form.reset();
+    this._userService.register(this.user).subscribe(
+      response => {
+        if(response.status== "success"){
+          this.status = response.status;
+          form.reset();
+
+        }else{
+          this.status = 'error';
+        }
+        
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    );
+    
+    
   }
 }
