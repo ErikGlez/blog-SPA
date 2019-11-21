97105142eded0038a57f3ec5774f460d285addce
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-11-20 17:31:10 -0700

    Mostrar usuario y logout del usuario.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
- 
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -17,51 +18,75 @@ export class LoginComponent implements OnInit {
   public identity;
 
   constructor(
-    private _userService: UserService
-    
-  ) { 
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+
+  ) {
     this.page_title = 'Identificate';
-    this.user = new User(1, '', '','ROLE_USER','','','','');
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
   }
 
   ngOnInit() {
+    // se ejecuta siempre y cierra sesion solo cuando le llega el parametro sure por la url
+    this.logout();
   }
 
-  onSubmit(form){
+  onSubmit(form) {
     this._userService.signup(this.user).subscribe(
-      response =>{
-          // TOKEN
-          if(response.status != 'error'){
-            this.status = 'success';
-            this.token = response;
+      response => {
+        // TOKEN
+        if (response.status != 'error') {
+          this.status = 'success';
+          this.token = response;
 
-            // OBJETO USUARIO IDENTIFICADO
-              this._userService.signup(this.user, true).subscribe(
-                response =>{
-                      this.identity = response;
-              
-                      //Persistir datos del usuario identificado
-                      console.log(this.token);
-                      console.log(this.identity);
-                      
-                      localStorage.setItem('token', this.token);
-                      localStorage.setItem('identity', JSON.stringify(this.identity));
-                  
-                }, 
-                error => {
-                  this.status = 'error';
-                  console.log(<any>error);
-                }
-              );
-          }else{
-            this.status = 'error';
-          }
-      }, 
+          // OBJETO USUARIO IDENTIFICADO
+          this._userService.signup(this.user, true).subscribe(
+            response => {
+              this.identity = response;
+
+              //Persistir datos del usuario identificado
+              console.log(this.token);
+              console.log(this.identity);
+
+              localStorage.setItem('token', this.token);
+              localStorage.setItem('identity', JSON.stringify(this.identity));
+
+              //Redireccion a inicio
+              this._router.navigate(['inicio']);
+
+            },
+            error => {
+              this.status = 'error';
+              console.log(<any>error);
+            }
+          );
+        } else {
+          this.status = 'error';
+        }
+      },
       error => {
         this.status = 'error';
         console.log(<any>error);
       }
     );
+  }
+
+  logout() {
+    this._route.params.subscribe(params => {
+      let logout = +params['sure'];
+
+      if (logout == 1) {
+        localStorage.removeItem('identity');
+        localStorage.removeItem('token');
+
+        this.identity = null;
+        this.token = null;
+
+        //Redireccion a inicio
+        this._router.navigate(['inicio']);
+      }
+    });
   }
 
 }
